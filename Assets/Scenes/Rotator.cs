3e28654866c0039f0e519be92e144f58611a480e
Abbas Khan <Andyad999@gmail.com>
2023-11-14 08:36:24 +0400

    environment setup

--- Assets/Scenes/Rotator.cs
+++ Assets/Scenes/Rotator.cs
@@ -6,31 +6,42 @@ public class Rotator : MonoBehaviour
 {
     [SerializeField] private Vector3 spinCloads;
     [SerializeField] private float speed = 1f;
-    [SerializeField] Light moonlight;
-    [SerializeField] GameObject proceedButton;
+    [SerializeField] private Light moonlight;
+    [SerializeField] private GameObject proceedButton;
     public Color startColor;
     public Color endColor;
+
+    private float rotationProgress = 0f;
 
     private void Start()
     {
         proceedButton.SetActive(false);
-        if (moonlight != null) moonlight.color= startColor;
+        if (moonlight != null) moonlight.color = startColor;
     }
+
     void Update()
     {
         transform.Rotate(spinCloads * speed * Time.deltaTime);
+
         if (moonlight == null) return;
-        // lerp moonlight between red and white, red if rotation.x is 170 and white if rotation.x is 100
-        if (transform.rotation.x > 170)
+
+        // Increment rotation progress over time
+        rotationProgress += speed * Time.deltaTime;
+
+        // Lerp moonlight between red and white, red if rotation.x is 170 and white if rotation.x is 100
+        float t = Mathf.InverseLerp(100f, 170f, rotationProgress);
+
+        moonlight.color = Color.Lerp(startColor, endColor, t);
+
+        if (rotationProgress > 170)
         {
-            moonlight.color = Color.Lerp(startColor, endColor, (transform.rotation.x - 170) / 30, Time.deltaTime);
             proceedButton.SetActive(true);
         }
-        else if (transform.rotation.x < 100)
+        else if (rotationProgress < 100)
         {
-            moonlight.color = Color.Lerp(endColor, startColor, transform.rotation.x / 100 , Time.deltaTime);
+            proceedButton.SetActive(false);
         }
     }
-
 }
+
 
